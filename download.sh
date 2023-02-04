fa4bcf0a2ea1d4da348703d18ae886aaaff809f9
ntua-el17044 <94133744+ntua-el17044@users.noreply.github.com>
2023-02-04 17:34:42 +0200

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,8 @@
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-01.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-02.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-03.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-04.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-05.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-06.parquet
+
+wget https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv
